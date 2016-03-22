1c58731a0bb970f96c5445ed5b838a209ddd441a
Tim Perry <pimterry@gmail.com>
2016-03-23 00:56:57 +0100

    Add tests for component nesting

--- test/test.js
+++ test/test.js
@@ -2,13 +2,13 @@ var expect = require('chai').expect;
 
 var serverComponents = require("../src/index.js");
 
-function bodyWrapped(content) {
+function body(content) {
     return "<html><head></head><body>" + content + "</body></html>";
 }
 
 describe("Server components", () => {
     it("does nothing with vanilla HTML", () => {
-        var input = bodyWrapped("<div></div>");
+        var input = body("<div></div>");
         var output = serverComponents.render(input);
         expect(output).to.equal(input);
     });
@@ -18,8 +18,28 @@ describe("Server components", () => {
         NewElement.createdCallback = function () { this.innerHTML = "hi there"; };
         serverComponents.registerElement("my-element", { prototype: NewElement });
 
-        var output = serverComponents.render(bodyWrapped("<my-element></my-element>"));
+        var output = serverComponents.render(body("<my-element></my-element>"));
 
-        expect(output).to.equal(bodyWrapped("<my-element>hi there</my-element>"));
+        expect(output).to.equal(body("<my-element>hi there</my-element>"));
+    });
+
+    it("can wrap existing content", () => {
+        var PrefixedElement = serverComponents.newElement();
+        PrefixedElement.createdCallback = function () { this.innerHTML = "prefix:" + this.innerHTML; };
+        serverComponents.registerElement("prefixed-element", { prototype: PrefixedElement });
+
+        var output = serverComponents.render(body("<prefixed-element>existing-content</prefixed-element>"));
+
+        expect(output).to.equal(body("<prefixed-element>prefix:existing-content</prefixed-element>"));
+    });
+
+    it("allows nested elements", () => {
+        var PrefixedElement = serverComponents.newElement();
+        PrefixedElement.createdCallback = function () { this.innerHTML = "prefix:" + this.innerHTML; };
+        serverComponents.registerElement("prefixed-element", { prototype: PrefixedElement });
+
+        var output = serverComponents.render(body("<prefixed-element><prefixed-element>existing-content</prefixed-element></prefixed-element>"));
+
+        expect(output).to.equal(body("<prefixed-element>prefix:<prefixed-element>prefix:existing-content</prefixed-element></prefixed-element>"));
     });
 });
