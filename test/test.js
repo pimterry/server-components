35d3a38d7b97e60a96891d4900d4442f31ddcec1
Tim Perry <pimterry@gmail.com>
2016-03-22 20:12:37 +0100

    Clarify tests a little

--- test/test.js
+++ test/test.js
@@ -2,9 +2,13 @@ var expect = require('chai').expect;
 
 var serverComponents = require("../src/index.js");
 
+function bodyWrapped(content) {
+    return "<html><head></head><body>" + content + "</body></html>";
+}
+
 describe("Server components", () => {
     it("does nothing with vanilla HTML", () => {
-        var input = "<html><head></head><body><div></div></body></html>";
+        var input = bodyWrapped("<div></div>");
         var output = serverComponents.render(input);
         expect(output).to.equal(input);
     });
@@ -14,8 +18,8 @@ describe("Server components", () => {
         NewElement.createdCallback = function () { this.innerHTML = "hi there"; };
         serverComponents.registerElement("my-element", { prototype: NewElement });
 
-        var output = serverComponents.render("<html><head></head><body><my-element></my-element></body></html>");
+        var output = serverComponents.render(bodyWrapped("<my-element></my-element>"));
 
-        expect(output).to.equal("<html><head></head><body><my-element>hi there</my-element></body></html>");
+        expect(output).to.equal(bodyWrapped("<my-element>hi there</my-element>"));
     });
 });
