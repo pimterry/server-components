2e008c71b0b9341632c1646f6da3d10350d3f095
Tim Perry <pimterry@gmail.com>
2016-05-10 20:58:26 +0200

    Do some basic documenting

--- src/index.js
+++ src/index.js
@@ -10,12 +10,29 @@ var validateElementName = require("validate-element-name");
  */
 exports.dom = domino.impl;
 
+/**
+ * Creates a returns a new custom HTML element prototype, extending the HTMLElement prototype.
+ *
+ * Note that this does *not* register the element. To do that, call ServerComponents.registerElement
+ * with an element name, and options (typically including the prototype returned here as your
+ * 'prototype' value).
+ */
 exports.newElement = function newElement() {
     return Object.create(domino.impl.HTMLElement.prototype);
 };
 
 var registeredElements = {};
 
+/**
+ * Registers an element, so that it will be used when the given element name is found during parsing.
+ *
+ * Element names are required to contain a hyphen (to disambiguate them from existing element names),
+ * be entirely lower-case, and not start with a hyphen.
+ *
+ * The only option currently supported is 'prototype', which sets the prototype of the given element.
+ * This prototype will have its various callbacks called when it is found during document parsing,
+ * and properties of the prototype will be exposed within the DOM to other elements there in turn.
+ */
 exports.registerElement = function registerElement(name, options) {
     var nameValidationResult = validateElementName(name);
     if (!nameValidationResult.isValid) {
@@ -32,6 +49,10 @@ function recurseTree(rootNode, callback) {
     }
 }
 
+/**
+ * Take a string of HTML input, and render it into a full page, handling any custom elements found
+ * within, and returning a promise for the resulting string of HTML.
+ */
 exports.render = function render(input) {
     let doc = domino.createDocument(input);
 
